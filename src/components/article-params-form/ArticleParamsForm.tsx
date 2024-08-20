49e78b47dca7b453e75039047033a4bf89eb3d74
Haelrail <mosalevaleksandr@gmail.com>
2024-08-20 14:57:40 +0300

    components added on page, reset and submit buttons done, have to fix outside clicks - they broke the form opening

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,127 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { RadioGroup } from '../radio-group';
+import { Select } from '../select';
+import { Separator } from '../separator';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { Dispatch, SetStateAction, useState, useRef, useEffect } from 'react';
+import { OptionType, backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+
+type ArticleData = {
+	fontType: OptionType;
+	setFontType: Dispatch<SetStateAction<OptionType>>;
+
+	fontSize: OptionType;
+	setFontSize: Dispatch<SetStateAction<OptionType>>;
+
+	fontColor: OptionType;
+	setFontColor: Dispatch<SetStateAction<OptionType>>;
+
+	backgroundColor: OptionType;
+	setBackgroundColor: Dispatch<SetStateAction<OptionType>>;
+
+	contentWidth: OptionType;
+	setContentWidth: Dispatch<SetStateAction<OptionType>>;
+
+	resetStyles: () => void;
+
+	submitStyles: () => void;
+}
+
+export const ArticleParamsForm = (props: ArticleData) => {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const  handleOpenButtonClick = () => {
+		setIsOpen(!isOpen);
+		console.log("Current isOpen:", isOpen);
+	};
+
+	const handleFontTypeSelection = (selectedFontType: OptionType) => {
+		props.setFontType(selectedFontType);
+	};
+
+	const handleFontSizeSelection = (selectedFontSize: OptionType) => {
+		props.setFontSize(selectedFontSize);
+	};
+
+	const handleFontColorSelection = (selectedFontColor: OptionType) => {
+		props.setFontColor(selectedFontColor)
+	};
+
+	const handleBackgroundColorSelection = (selectedBackgroundColor: OptionType) => {
+		props.setBackgroundColor(selectedBackgroundColor)
+	};
+
+	const handleContentWidthSelection = (selectedContentWidth: OptionType) => {
+		props.setContentWidth(selectedContentWidth)
+	};
+
+	const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		props.submitStyles();
+	};
+
+	const formRef = useRef<HTMLElement>(null);
+
+	const handleClickOutOfForm = (event: MouseEvent) => {
+		if (formRef.current && !formRef.current.contains(event.target as Node)) {
+		  setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		if(isOpen)
+			document.addEventListener('click', handleClickOutOfForm);
+		else
+			document.removeEventListener('click', handleClickOutOfForm)
+		return () => {
+			document.removeEventListener('click', handleClickOutOfForm);
+			};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={handleOpenButtonClick}  isOpen={isOpen}/>
+			<aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`} ref={formRef}>
+				<form className={styles.form} onSubmit={handleSubmitForm}>
+					<Select
+						selected = {props.fontType}
+						options = {fontFamilyOptions}
+						onChange = {handleFontTypeSelection}
+						title='шрифт'
+					/>
+					<RadioGroup
+						name=''
+						options={fontSizeOptions}
+						selected={props.fontSize}
+						title='размер шрифта'
+						onChange={handleFontSizeSelection}
+					/>
+					<Select
+						selected={props.fontColor}
+						options={fontColors}
+						onChange={handleFontColorSelection}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={props.backgroundColor}
+						options={backgroundColors}
+						onChange={handleBackgroundColorSelection}
+						title='цвет фона'
+					/>
+					<Select
+						selected={props.contentWidth}
+						options={contentWidthArr}
+						onChange={handleContentWidthSelection}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={props.resetStyles}/>
+						<Button title='Применить' type='submit'/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { OptionType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,84 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+
+	// отображают выбранные пользователем стили в форме
+
+	const [fontType, setFontType] = useState<OptionType>(defaultArticleState.fontFamilyOption);
+
+	const [fontSize, setFontSize] = useState<OptionType>(defaultArticleState.fontSizeOption);
+
+	const [fontColor, setFontColor] = useState<OptionType>(defaultArticleState.fontColor);
+
+	const [backgroundColor, setBackgroundColor] = useState<OptionType>(defaultArticleState.backgroundColor);
+
+	const [contentWidth, setContentWidth] = useState<OptionType>(defaultArticleState.contentWidth);
+
+	// рендерит компонент с примененными пользователем стилями
+
+	const [selectedOptions, setSelectedOptions] = useState({
+		fontType: defaultArticleState.fontFamilyOption.value,
+		fontSize: defaultArticleState.fontSizeOption.value,
+		fontColor: defaultArticleState.fontColor.value,
+		backgroundColor: defaultArticleState.backgroundColor.value,
+		contentWidth: defaultArticleState.contentWidth.value
+	})
+
+	// сбрасывает активные стили и "выбранные" стили в форме на дефолтные
+
+	const resetStyles = () => {
+		setFontType(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		setSelectedOptions({
+			fontType: defaultArticleState.fontFamilyOption.value,
+			fontSize: defaultArticleState.fontSizeOption.value,
+			fontColor: defaultArticleState.fontColor.value,
+			backgroundColor: defaultArticleState.backgroundColor.value,
+			contentWidth: defaultArticleState.contentWidth.value
+		});
+	}
+
+	// применяет выбранные стили к компоненту
+
+	const submitStyles = () => {
+		setSelectedOptions({
+			fontType: fontType.value,
+			fontSize: fontSize.value,
+			fontColor: fontColor.value,
+			backgroundColor: backgroundColor.value,
+			contentWidth: contentWidth.value
+		})
+	}
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': selectedOptions.fontType,
+					'--font-size': selectedOptions.fontSize,
+					'--font-color': selectedOptions.fontColor,
+					'--container-width': selectedOptions.contentWidth,
+					'--bg-color': selectedOptions.backgroundColor,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				fontType={fontType}
+				setFontType={setFontType}
+				fontSize={fontSize}
+				setFontSize={setFontSize}
+				fontColor={fontColor}
+				setFontColor={setFontColor}
+				backgroundColor={backgroundColor}
+				setBackgroundColor={setBackgroundColor}
+				contentWidth={contentWidth}
+				setContentWidth={setContentWidth}
+				resetStyles={resetStyles}
+				submitStyles={submitStyles}
+			/>
 			<Article />
 		</div>
 	);
